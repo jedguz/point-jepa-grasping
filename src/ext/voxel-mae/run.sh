296cc3019cdf46840f094f8c47621de1ec308482
Jed <jed.guzelkabaagac@tum.de>
2025-05-29 13:35:03 +0000

    del voxel-mae

--- src/ext/voxel-mae/run.sh
+++
@@ -1,2 +0,0 @@
-CONFIG=sst_nuscenes_masked
-bash tools/dist_train.sh configs/sst_refactor/$CONFIG.py 8 --work-dir ./work_dirs/$CONFIG/ --cfg-options evaluation.pklfile_prefix=./work_dirs/$CONFIG/results evaluation.metric=nuscenes
