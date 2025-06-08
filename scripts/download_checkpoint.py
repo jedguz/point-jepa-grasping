a0938547d5774580d4c82b966e33669b5ce2a036
Boris Petrovic <boris120900@gmail.com>
2025-06-08 10:09:07 +0000

    Cleaning up the working directory

--- scripts/download_checkpoint.py
+++
@@ -1,9 +0,0 @@
-# scripts/download_checkpoint.py
-import os, requests
-url = 'https://github.com/georghess/voxel-mae/releases/download/v0.1/voxelmae_nuscenes.pth'
-out_dir = 'ext/voxel-mae/pretrained'
-os.makedirs(out_dir, exist_ok=True)
-r = requests.get(url, allow_redirects=True)
-with open(os.path.join(out_dir, 'voxelmae_nuscenes.pth'), 'wb') as f:
-    f.write(r.content)
-print('Downloaded checkpoint to', out_dir)
